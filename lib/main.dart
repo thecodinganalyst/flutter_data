bc9ef14b198bf11402707c37ff9afcf7b7e9ab64
Dennis Cai <dennis.cai@zuhlke.com>
2022-11-10 11:46:23 +0800

    get full album lists

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 
+const AlbumsApi = 'https://jsonplaceholder.typicode.com/albums';
+
 class Album {
   final int userId;
   final int id;
@@ -24,8 +26,19 @@ class Album {
   }
 }
 
-Future<Album> fetchAlbum() async {
-  final response = await http.get(Uri.parse('https://jsonplaceholder.typicode.com/albums/1'));
+Future<List<Album>> fetchAlbumList() async {
+  final response = await http.get(Uri.parse('$AlbumsApi'));
+
+  if(response.statusCode == 200){
+    Iterable list = jsonDecode(response.body);
+    return List<Album>.from(list.map((json) => Album.fromJson(json)));
+  }else{
+    throw Exception('Failed to load albums');
+  }
+}
+
+Future<Album> fetchAlbum(int id) async {
+  final response = await http.get(Uri.parse('$AlbumsApi/$id'));
 
   if(response.statusCode == 200) {
     return Album.fromJson(jsonDecode(response.body));
@@ -46,12 +59,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late Future<Album> futureAlbum;
+  late Future<List<Album>> futureAlbumList;
 
   @override
   void initState(){
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureAlbumList = fetchAlbumList();
   }
 
   @override
@@ -65,20 +78,43 @@ class _MyAppState extends State<MyApp> {
         appBar: AppBar(
           title: const Text('Fetch Data Example'),
         ),
-        body: Center(
-          child: FutureBuilder<Album>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
-              if(snapshot.hasData) {
-                return Text(snapshot.data!.title);
-              }else if(snapshot.hasError) {
-                return Text('${snapshot.error}');
-              }
-              return const CircularProgressIndicator();
-            },
-          ),
+        body: FutureBuilder<List<Album>>(
+          future: futureAlbumList,
+          builder: (context, snapshot) {
+            if(snapshot.hasData) {
+              return AlbumListView(albumList: snapshot.data!);
+            }else if(snapshot.hasError) {
+              return Center(
+                child: Text('${snapshot.error}'),
+              );
+            }
+            return const CircularProgressIndicator();
+          },
         ),
       ),
+    );
+  }
+}
+
+class AlbumListView extends StatelessWidget {
+  final List<Album> albumList;
+
+  const AlbumListView({super.key, required this.albumList});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: albumList.length,
+      itemBuilder: (context, i) {
+        final album = albumList[i];
+        return ListTile(
+          leading: Text(album.id.toString()),
+          title: Text(album.title),
+        );
+      },
+      separatorBuilder: (context, i) {
+        return Divider();
+      },
     );
   }
 }
